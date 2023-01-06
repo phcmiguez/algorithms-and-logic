6c2e9fd082217bd75a8270bc315f606af7c0ba2c
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-01-06 18:58:49 -0300

    Celsius to Fahrenheit

---
+++ challenge04.js
@@ -0,0 +1,15 @@
+// Celsius to Fahrenheith
+
+function celsiusToFahrenheit(celsius){
+  return (celsius * 9) / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(25));
+
+// Fahrenheit to Celsius
+
+function fahrenheitToCelsius(fahrenheit){
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+console.log(fahrenheitToCelsius(77));
