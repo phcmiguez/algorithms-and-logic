03961744b5acee895615042753f4e5446c0eca3e
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-01-06 13:53:52 -0300

    pi to PI

--- challenge02.js
+++ challenge02.js
@@ -1,10 +1,10 @@
 // Calculate a circle area
 
 function circleArea(radius) {
-  const pi = 3.14159265359;
+  const PI = 3.14159265359;
   const diameter = (radius ** 2);
-  const area = diameter * pi;
+  const area = diameter * PI;
   return 'The area of the circle is ' + area.toFixed(1) + ' square meters. The diameter is ' + diameter.toFixed(2) + ' meters.';
 }
 
-console.log(circleArea(16));
+console.log(circleArea(43));
