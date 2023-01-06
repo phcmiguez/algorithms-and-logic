86316ed6e729dd949c42d4f1ba58a5ff4e6b12fe
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-01-06 19:03:54 -0300

    Added text to return

--- challenge04.js
+++ challenge04.js
@@ -1,15 +1,17 @@
 // Celsius to Fahrenheith
 
 function celsiusToFahrenheit(celsius){
-  return (celsius * 9) / 5 + 32;
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return celsius + 'ºC is equal to ' + fahrenheit.toFixed(2) + 'ºF.'
 }
 
-console.log(celsiusToFahrenheit(25));
+console.log(celsiusToFahrenheit(-19));
 
 // Fahrenheit to Celsius
 
 function fahrenheitToCelsius(fahrenheit){
-  return (fahrenheit - 32) * 5 / 9;
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  return fahrenheit + 'ºF is equal to ' + celsius.toFixed(2) + 'ºC.'
 }
 
-console.log(fahrenheitToCelsius(77));
+console.log(fahrenheitToCelsius(212));
