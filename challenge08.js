40314f63576d99978bd22ca92f006518d3bfae9a
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-01-12 19:51:35 -0300

    Constructor

--- challenge08.js
+++ challenge08.js
@@ -7,6 +7,33 @@ const marryDate = {
   showDate: function () {
     return `${this.day}/${this.month}/${this.year}`; // Br format
   }
-}
+};
 
 console.log(marryDate.showDate());
+
+//Methods reuse
+function createDate(day, month, year){
+  return {
+    day,
+    month,
+    year,
+    showDate() {
+      return `${this.day}/${this.month}/${this.year}`;
+    }
+  };
+};
+
+const birthDate = {
+  day: 23,
+  month: 11,
+  year: 1984,
+};
+
+const graduateDate = {
+  day: 18,
+  month: 09,
+  year: 2018,
+};
+
+console.log(createDate(birthDate.day, birthDate.month, birthDate.year).showDate());
+console.log(createDate(30,09,2017).showDate());
---
+++ constructorFunction.js
@@ -0,0 +1,31 @@
+//Constructor function
+
+// class Product {
+//   name = 'iPad';
+//   model = 'A28M1';
+//   year = 2023;
+//   turnOn = () => {
+//     return 'Online';
+//   }
+// };
+
+// const i = new Product;
+
+// console.log(i.turnOn());
+
+function ProductDate(year = 2023, month = 1, day = 1){
+  this.year = year;
+  this.month = month;
+  this.day = day;
+  this.showDate = function(){
+    return `${this.year}-${this.month}-${this.day}`;
+  }
+}
+
+const p1 = new ProductDate();
+const p2 = new ProductDate(2013, 07, 25);
+const p3 = new ProductDate(2017, 09, 30);
+
+console.log(p1.showDate());
+console.log(p2.showDate());
+console.log(p3.showDate());
